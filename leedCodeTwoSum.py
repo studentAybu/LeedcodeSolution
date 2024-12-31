62e676f7169dd71475f7a2f3948c5b08d29c2731
studentAybu <23050161004@aybu.edu.tr>
2024-12-31 14:53:30 +0300

    second

--- leedCodeTwoSum.py
+++ leedCodeTwoSum.py
@@ -1,8 +1,10 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target) :
+       
         numDictionary = {}
         for i in range(len(nums)):
             if target - nums[i] in numDictionary:
                 return [numDictionary[target - nums[i]], i]
             else:
                 numDictionary[nums[i]] = i
+print("test")
